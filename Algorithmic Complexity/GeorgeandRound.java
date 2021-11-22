9992db7dc003beb0938062003b58573651ac1892
ynguyen0610 <79205597+ynguyen0610@users.noreply.github.com>
2021-11-22 15:34:23 -0500

    Update GeorgeandRound.java

--- Algorithmic Complexity/GeorgeandRound.java
+++ Algorithmic Complexity/GeorgeandRound.java
@@ -55,3 +55,21 @@ public class Main {
     System.out.println(output);
   }
 }
+/*
+Python:
+n, m = map(int, input().split())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+ 
+count = i = 0
+ 
+for j in range(m):
+    if i >= n:
+        break
+ 
+    if b[j] >= a[i]:
+        count += 1
+        i += 1
+ 
+print(n - count)
+*/
